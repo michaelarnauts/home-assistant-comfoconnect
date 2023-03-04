711ee177a5e415f9c1662edf97493513cf96bda1
Michaël Arnauts <michael.arnauts@gmail.com>
2023-03-04 12:32:27 +0100

    Use async_forward_entry_setups instead of async_setup_platforms. Fixes #7.

--- custom_components/comfoconnect/__init__.py
+++ custom_components/comfoconnect/__init__.py
@@ -98,7 +98,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         via_device=(DOMAIN, bridge_info.serialNumber),
     )
 
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     @callback
     async def send_keepalive(now) -> None:
