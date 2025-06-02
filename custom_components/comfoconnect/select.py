1967b6a13d063ca8e660b14eb310395c87e237a9
Dave King <la00075@LA00075s-MBP.localdomain>
2025-06-02 09:57:18 -0700

    Fix code formatting with ruff

--- custom_components/comfoconnect/select.py
+++ custom_components/comfoconnect/select.py
@@ -144,9 +144,7 @@ SELECT_TYPES = (
         icon="mdi:fan-plus",
         get_value_fn=lambda ccb: cast(Coroutine, ccb.get_boost()),
         set_value_fn=lambda ccb, option: cast(Coroutine, ccb.set_boost(True, int(option.split()[0]) * 60)),
-        options=[
-            "10 Minutes", "20 Minutes", "30 Minutes", "40 Minutes", "50 Minutes", "60 Minutes"
-        ],
+        options=["10 Minutes", "20 Minutes", "30 Minutes", "40 Minutes", "50 Minutes", "60 Minutes"],
     ),
 )
 
