21f6c5ec6abc38e4a603f4c331537a6cf420ea08
Nerdix <70015952+N3rdix@users.noreply.github.com>
2025-09-28 11:37:04 +0200

    Add Boost Mode option "Off"

--- custom_components/comfoconnect/select.py
+++ custom_components/comfoconnect/select.py
@@ -143,8 +143,10 @@ SELECT_TYPES = (
         name="Boost Mode",
         icon="mdi:fan-plus",
         get_value_fn=lambda ccb: cast(Coroutine, ccb.get_boost()),
-        set_value_fn=lambda ccb, option: cast(Coroutine, ccb.set_boost(True, int(option.split()[0]) * 60)),
-        options=["10 Minutes", "20 Minutes", "30 Minutes", "40 Minutes", "50 Minutes", "60 Minutes"],
+        set_value_fn=lambda ccb, option: (
+        cast(Coroutine, ccb.set_boost(False)) if option == "Off"
+        else cast(Coroutine, ccb.set_boost(True, int(option.split()[0]) * 60))),
+        options=["Off", "10 Minutes", "20 Minutes", "30 Minutes", "40 Minutes", "50 Minutes", "60 Minutes"],
     ),
 )
 
